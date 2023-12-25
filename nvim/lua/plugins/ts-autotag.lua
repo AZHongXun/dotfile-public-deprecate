8e996ebc8ec6208471119a98540d5add944dcbbb
HongXun <90100913+AZHongXun@users.noreply.github.com>
2023-12-25 22:55:27 +0800

    Delete nvim directory

--- nvim/lua/plugins/ts-autotag.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'windwp/nvim-ts-autotag',
-  ft = { "html", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-  config = function()
-    require('nvim-ts-autotag').setup({
-    })
-  end
-}
