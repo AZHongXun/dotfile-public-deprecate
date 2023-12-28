aa0e3ce270f75b63e89745427dbb909b70d364e5
HongXun <90100913+AZHongXun@users.noreply.github.com>
2023-12-28 19:42:51 +0800

    add diagnostic from nvim-lsp

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -16,6 +16,12 @@ return {
 				show_buffer_close_icons = false,
 				show_close_icon = false,
 				color_icons = true,
+				diagnostics = "nvim_lsp",
+				diagnostics_update_in_insert = false,
+				diagnostics_indicator = function(count, level)
+					local icon = level:match("error") and " " or " "
+					return " " .. icon .. count
+				end,
 			},
 			highlights = {
 				separator = {
