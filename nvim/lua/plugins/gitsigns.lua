2d2e4766fa8fccd9374633e8cc1d5a2378d2924e
HongXun <90100913+AZHongXun@users.noreply.github.com>
2023-12-28 19:46:49 +0800

    delete the redundant options

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -17,33 +17,6 @@ return {
 		})
 	end,
 	opts = {
-		signs = {
-			add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-			change = {
-				hl = "GitSignsChange",
-				text = "▎",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
-			delete = {
-				hl = "GitSignsDelete",
-				text = "",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			topdelete = {
-				hl = "GitSignsDelete",
-				text = "",
-				numhl = "GitSignsDeleteNr",
-				linehl = "GitSignsDeleteLn",
-			},
-			changedelete = {
-				hl = "GitSignsChange",
-				text = "▎",
-				numhl = "GitSignsChangeNr",
-				linehl = "GitSignsChangeLn",
-			},
-		},
 		signcolumn = true,
 		watch_gitdir = {
 			interval = 1000,
