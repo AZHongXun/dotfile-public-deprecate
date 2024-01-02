1d50e29fbb3b3f4383c864ed1e22cbdfcd4e3dc5
HongXun <90100913+AZHongXun@users.noreply.github.com>
2024-01-02 16:05:45 +0800

    correct typo

--- nvim/lua/util/lsp.lua
+++ nvim/lua/util/lsp.lua
@@ -1,22 +1,24 @@
 local M = {}
 
 M.on_attach = function(client, bufnr)
-	local opts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-	vim.keymap.set("n", "gl", "<Cmd>Lspsaga show_line_diagnostics<CR>", opts)
-	vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
-	vim.keymap.set("n", "gd", "<Cmd>Lspsaga lsp_finder<CR>", opts)
-	vim.keymap.set("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	vim.keymap.set("n", "gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
-	vim.keymap.set("n", "<leader>cr", "<Cmd>Lspsaga rename<CR>", opts)
-	vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>") -- code action
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+  vim.keymap.set("n", "gl", "<Cmd>Lspsaga show_line_diagnostics<CR>", opts)
+  vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
+  vim.keymap.set("n", "gd", "<Cmd>Lspsaga finder<CR>", opts)
+  vim.keymap.set("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  vim.keymap.set("n", "gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
+  vim.keymap.set("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
+  vim.keymap.set({ "n", "v" }, "ga", "<cmd>Lspsaga code_action<CR>") -- code action
+  vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+  vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>")
 end
 
 M.capabilities = function()
-	local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-	if status_ok then
-		return cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
-	end
+  local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+  if status_ok then
+    return cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  end
 end
 
 return M
