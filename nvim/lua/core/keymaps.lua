fc35f5d1ab3064f0ab60b8c118b5d893b6a0c980
HongXun <90100913+AZHongXun@users.noreply.github.com>
2024-01-02 15:38:33 +0800

    disable arrow key in normal & remove some mappings

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,23 +1,20 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
+-- Disable arrow key in normal
+keymap("n", "<left>", "<Nop>", opts)
+keymap("n", "<right>", "<Nop>", opts)
+keymap("n", "<up>", "<Nop>", opts)
+keymap("n", "<down>", "<Nop>", opts)
+
 -- Disable X to cut
 keymap("n", "x", '"_x')
-
--- Move while insert
-keymap("i", "<C-h>", "<Left>", opts)
-keymap("i", "<C-j>", "<Down>", opts)
-keymap("i", "<C-k>", "<Up>", opts)
-keymap("i", "<C-l>", "<Right>", opts)
 
 -- Better escape in terminal mode
 keymap("t", "<C-[>", "<C-\\><C-n>")
 
 -- Toggle relative number
 keymap("n", "<leader>rn", "<cmd>set rnu!<cr>", { desc = "Toggle relative number" }, opts)
-
--- Format
-keymap("n", "<C-f>", "<cmd>lua vim.lsp.buf.format{async = true}<cr>", opts)
 
 -- Increment/decrement
 keymap("n", "+", "<C-a>")
@@ -40,11 +37,10 @@ keymap("n", "ss", "<cmd>split<Return><C-w>w")
 keymap("n", "sv", "<cmd>vsplit<Return><C-w>w")
 -- Move window
 keymap("n", "<Space>", "<C-w>w")
-keymap("", "sh", "<C-w>h")
-keymap("", "sk", "<C-w>k")
-keymap("", "sj", "<C-w>j")
-keymap("", "sl", "<C-w>l")
-
+keymap("n", "sh", "<C-w>h")
+keymap("n", "sk", "<C-w>k")
+keymap("n", "sj", "<C-w>j")
+keymap("n", "sl", "<C-w>l")
 -- Resize window
 keymap("n", "<C-w><left>", "<C-w><")
 keymap("n", "<C-w><right>", "<C-w>>")
