ef907b47e5ec7b33b5031a3541097ea926e39435
HongXun <90100913+AZHongXun@users.noreply.github.com>
2024-01-03 01:01:33 +0800

    change pre_hook opts

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,22 +1,24 @@
 return {
-	"numToStr/Comment.nvim",
-	dependencies = {
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		lazy = true,
-		opts = { enable_autocmd = false },
-	},
-	keys = {
-		{ "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<cr>", desc = "Comment" },
-		{
-			"<leader>/",
-			'<esc><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<cr>',
-			desc = "Comment",
-			mode = "x",
-		},
-	},
-	opts = {
-		custom_commentstring = function()
-			return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-		end,
-	},
+  "numToStr/Comment.nvim",
+  dependencies = {
+    {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      opts = { enable_autocmd = false },
+    },
+    "nvim-treesitter/nvim-treesitter"
+  },
+  keys = {
+    { "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<cr>", desc = "Comment" },
+    {
+      "<leader>/",
+      '<esc><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<cr>',
+      desc = "Comment",
+      mode = "x",
+    },
+  },
+  config = function()
+    require("Comment").setup({
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+    })
+  end
 }
